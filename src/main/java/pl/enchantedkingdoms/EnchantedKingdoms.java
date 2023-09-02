7e00fd449fa2d0876bbcbc489f3fad9cc845f767
Melzak252 <melzacki.jakub@gmail.com>
2023-09-02 13:57:53 +0200

    Configure environment

--- src/main/java/pl/enchantedkingdoms/EnchantedKingdoms.java
+++ src/main/java/pl/enchantedkingdoms/EnchantedKingdoms.java
@@ -1,9 +1,10 @@
-package com.protonmail.enchantedkingdoms;
+package pl.enchantedkingdoms;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -26,6 +27,7 @@ public class EnchantedKingdoms
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
@@ -36,6 +38,11 @@ public class EnchantedKingdoms
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event){
 
     }
 
